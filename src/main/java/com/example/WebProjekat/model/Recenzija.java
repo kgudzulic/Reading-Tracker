1cb0c3373e47e610830651af3be9cdc91b1b0f23
Kristina Gudzulic <gudzulic.kristina@gmail.com>
2023-04-07 16:08:02 +0200

    Izmene u klasama Recenzija i Zahtev za aktivaciju, dodata klasa za Status aktivacije

--- src/main/java/com/example/WebProjekat/model/Recenzija.java
+++ src/main/java/com/example/WebProjekat/model/Recenzija.java
@@ -2,9 +2,23 @@ package com.example.WebProjekat.model;
 
 import jakarta.persistence.*;
 import java.io.Serializable;
+import java.util.Date;
+
 @Entity
 public class Recenzija implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
+    private Long ocena;
+
+    @Column
+    private String tekst;
+
+    @Column
+    private Date datumRecenzije;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Korisnik korisnik;
 }
--- src/main/java/com/example/WebProjekat/model/StatusAktivacije.java
+++ src/main/java/com/example/WebProjekat/model/StatusAktivacije.java
@@ -2,11 +2,13 @@ package com.example.WebProjekat.model;
 
 import jakarta.persistence.*;
 import java.io.Serializable;
+
 @Entity
-public class ZahtevAktivacijeAutora implements Serializable {
+public class StatusAktivacije implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
+    @Column
+    private String naziv;
 }

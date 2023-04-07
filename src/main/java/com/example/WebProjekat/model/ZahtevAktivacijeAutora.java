88d6a94de768ea03b2fe04d2bd35d4ea580c1cb5
Kristina Gudzulic <gudzulic.kristina@gmail.com>
2023-04-07 16:12:25 +0200

    Izmena klase zahtev za aktivaciju, povezan sa statusom

--- src/main/java/com/example/WebProjekat/model/ZahtevAktivacijeAutora.java
+++ src/main/java/com/example/WebProjekat/model/ZahtevAktivacijeAutora.java
@@ -2,11 +2,26 @@ package com.example.WebProjekat.model;
 
 import jakarta.persistence.*;
 import java.io.Serializable;
+import java.util.Date;
+
 @Entity
 public class ZahtevAktivacijeAutora implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column
+    private String email;
 
+    @Column
+    private String telefon;
+
+    @Column
+    private String poruka;
+
+    @Column
+    private Date datumZahteva;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private StatusAktivacije statusAktivacije;
 }

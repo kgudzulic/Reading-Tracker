801c6435921f1ab4af962819482eaec1ff206386
Kristina Gudzulic <gudzulic.kristina@gmail.com>
2023-04-21 09:15:27 +0200

    Dodavanje zanrova u data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO ZANR (naziv) VALUES ('Horor');
+INSERT INTO ZANR (naziv) VALUES ('Triler');
+INSERT INTO ZANR (naziv) VALUES ('Romantika');
+INSERT INTO ZANR (naziv) VALUES ('Kuvar');
+INSERT INTO ZANR (naziv) VALUES ('Komedija');
+INSERT INTO ZANR (naziv) VALUES ('Biografija');
+INSERT INTO ZANR (naziv) VALUES ('Naucna fantastika');
+INSERT INTO ZANR (naziv) VALUES ('Psiholoski');
+INSERT INTO ZANR (naziv) VALUES ('Poezija');
+

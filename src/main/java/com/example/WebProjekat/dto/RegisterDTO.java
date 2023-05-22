7a6e524c15f2ee5c5c621cc3d7beddd44132a968
Kristina Gudzulic <gudzulic.kristina@gmail.com>
2023-05-22 20:11:46 +0200

    Dodat RegisterDTO

---
+++ src/main/java/com/example/WebProjekat/dto/RegisterDTO.java
@@ -0,0 +1,90 @@
+package com.example.WebProjekat.dto;
+
+import com.example.WebProjekat.model.Uloga;
+
+import java.util.Date;
+
+public class RegisterDTO {
+    private String korisnickoIme;
+
+    private String lozinka;
+
+    private String ime;
+
+    private String prezime;
+
+    private String email;
+
+    private Date datumRodjenja = new Date();
+
+    private Uloga uloga;
+
+    public RegisterDTO() {
+    }
+
+    public RegisterDTO(String korisnickoIme, String lozinka, String ime, String prezime, String email, Date datumRodjenja, Uloga uloga) {
+        this.korisnickoIme = korisnickoIme;
+        this.lozinka = lozinka;
+        this.ime = ime;
+        this.prezime = prezime;
+        this.email = email;
+        this.datumRodjenja = datumRodjenja;
+        this.uloga = uloga;
+    }
+
+    public String getKorisnickoIme() {
+        return korisnickoIme;
+    }
+
+    public void setKorisnickoIme(String korisnickoIme) {
+        this.korisnickoIme = korisnickoIme;
+    }
+
+    public String getLozinka() {
+        return lozinka;
+    }
+
+    public void setLozinka(String lozinka) {
+        this.lozinka = lozinka;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDatumRodjenja() {
+        return datumRodjenja;
+    }
+
+    public void setDatumRodjenja(Date datumRodjenja) {
+        this.datumRodjenja = datumRodjenja;
+    }
+
+    public Uloga getUloga() {
+        return uloga;
+    }
+
+    public void setUloga(Uloga uloga) {
+        this.uloga = uloga;
+    }
+}

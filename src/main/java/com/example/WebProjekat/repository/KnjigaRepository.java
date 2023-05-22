1996955da4663975e027c1cd893a5950265386f8
Kristina Gudzulic <gudzulic.kristina@gmail.com>
2023-05-22 20:17:30 +0200

    Dodato listanje knjiga po naslovu ili po opisu

--- src/main/java/com/example/WebProjekat/repository/KnjigaRepository.java
+++ src/main/java/com/example/WebProjekat/repository/KnjigaRepository.java
@@ -2,6 +2,12 @@ package com.example.WebProjekat.repository;
 
 import com.example.WebProjekat.model.Knjiga;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
 public interface KnjigaRepository extends JpaRepository<Knjiga, Long>{
+    List<Knjiga> findAllByNaslovOrOrderById(String naslov);
+    List<Knjiga> findAllByOpisOrderById(String opis);
 }
 

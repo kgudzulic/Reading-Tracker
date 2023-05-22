dce1468bc788b6354441ee7dc29f8f168ffa4e52
Kristina Gudzulic <gudzulic.kristina@gmail.com>
2023-05-22 19:59:22 +0200

    Dodate posebne klase Administrator i Citalac

---
+++ src/main/java/com/example/WebProjekat/model/Administrator.java
@@ -0,0 +1,21 @@
+package com.example.WebProjekat.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+@Entity
+@DiscriminatorValue("ADMINISTRATOR")
+public class Administrator extends Korisnik implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Administrator(String ime, String prezime) {
+        super(ime, prezime);
+        this.id = id;
+    }
+
+    public Administrator() {
+
+    }
+}
---
+++ src/main/java/com/example/WebProjekat/model/Citalac.java
@@ -0,0 +1,22 @@
+package com.example.WebProjekat.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@DiscriminatorValue("CITALAC")
+public class Citalac extends Korisnik implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Citalac(String ime, String prezime) {
+        super(ime, prezime);
+        this.id = id;
+    }
+
+    public Citalac() {
+
+    }
+}

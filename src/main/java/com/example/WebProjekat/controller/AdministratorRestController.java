466aa79896673cda188231dbbf5451f636524b6c
Kristina Gudzulic <gudzulic.kristina@gmail.com>
2023-06-09 13:34:45 +0200

    Izmene administratora

--- src/main/java/com/example/WebProjekat/controller/AdministratorRestController.java
+++ src/main/java/com/example/WebProjekat/controller/AdministratorRestController.java
@@ -1,4 +1,10 @@
 package com.example.WebProjekat.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AdministratorRestController {
+    @Autowired
+    private AdministratorS
 }

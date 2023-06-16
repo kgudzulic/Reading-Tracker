3459fe6b01584dcdc7d73b8f3f3d2d01b5c24939
Kristina Gudzulic <gudzulic.kristina@gmail.com>
2023-06-16 21:24:39 +0200

    Izmena profila DTO

--- src/main/java/com/example/WebProjekat/dto/IzmenaProfilaDTO.java
+++ src/main/java/com/example/WebProjekat/dto/IzmenaProfilaDTO.java
@@ -1,4 +1,61 @@
 package com.example.WebProjekat.dto;
 
+import com.example.WebProjekat.model.EnumUloga;
+import com.example.WebProjekat.model.Polica;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
 public class IzmenaProfilaDTO {
+
+    private String ime;
+
+    private String prezime;
+
+    private Date datumRodjenja;
+
+    private String opis;
+
+    private String slika;
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public Date getDatumRodjenja() {
+        return datumRodjenja;
+    }
+
+    public void setDatumRodjenja(Date datumRodjenja) {
+        this.datumRodjenja = datumRodjenja;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+
+    public String getSlika() {
+        return slika;
+    }
+
+    public void setSlika(String slika) {
+        this.slika = slika;
+    }
 }

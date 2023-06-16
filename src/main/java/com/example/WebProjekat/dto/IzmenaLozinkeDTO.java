0d87c3123cea5774344796ef399e80301dc3c314
Kristina Gudzulic <gudzulic.kristina@gmail.com>
2023-06-16 21:26:41 +0200

    Izmena lozinke DTO

--- src/main/java/com/example/WebProjekat/dto/IzmenaLozinkeDTO.java
+++ src/main/java/com/example/WebProjekat/dto/IzmenaLozinkeDTO.java
@@ -1,4 +1,33 @@
 package com.example.WebProjekat.dto;
 
 public class IzmenaLozinkeDTO {
+    private String lozinka;
+
+    private String lozinkaPotvrda;
+
+    private String staraLozinka;
+
+    public String getLozinka() {
+        return lozinka;
+    }
+
+    public void setLozinka(String lozinka) {
+        this.lozinka = lozinka;
+    }
+
+    public String getLozinkaPotvrda() {
+        return lozinkaPotvrda;
+    }
+
+    public void setLozinkaPotvrda(String lozinkaPotvrda) {
+        this.lozinkaPotvrda = lozinkaPotvrda;
+    }
+
+    public String getStaraLozinka() {
+        return staraLozinka;
+    }
+
+    public void setStaraLozinka(String staraLozinka) {
+        this.staraLozinka = staraLozinka;
+    }
 }

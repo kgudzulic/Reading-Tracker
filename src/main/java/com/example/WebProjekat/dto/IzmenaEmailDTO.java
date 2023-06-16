456578fc5f0c69986b6cb749668d82ded13fe085
Kristina Gudzulic <gudzulic.kristina@gmail.com>
2023-06-16 21:28:42 +0200

    Izmena email DTO

--- src/main/java/com/example/WebProjekat/dto/IzmenaEmailDTO.java
+++ src/main/java/com/example/WebProjekat/dto/IzmenaEmailDTO.java
@@ -1,4 +1,23 @@
 package com.example.WebProjekat.dto;
 
 public class IzmenaEmailDTO {
+    private String email;
+
+    private String staraLozinka;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStaraLozinka() {
+        return staraLozinka;
+    }
+
+    public void setStaraLozinka(String staraLozinka) {
+        this.staraLozinka = staraLozinka;
+    }
 }

cc1ac056d9d51d7e9d06a7d42984ce3fcc4ba496
Kristina Gudzulic <gudzulic.kristina@gmail.com>
2023-06-17 08:39:07 +0200

    Sredjivanje zanr controllera

--- src/main/java/com/example/WebProjekat/controller/ZanrRestController.java
+++ src/main/java/com/example/WebProjekat/controller/ZanrRestController.java
@@ -1,4 +1,24 @@
 package com.example.WebProjekat.controller;
 
+
+import com.example.WebProjekat.model.Zanr;
+import com.example.WebProjekat.service.ZanrService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/zanrovi")
 public class ZanrRestController {
+    @Autowired
+    ZanrService zanrService;
+
+    @GetMapping
+    public ResponseEntity<List<Zanr>> get(){
+        return ResponseEntity.ok(zanrService.findAll());
+    }
 }
--- src/main/java/com/example/WebProjekat/model/Zanr.java
+++ src/main/java/com/example/WebProjekat/model/Zanr.java
@@ -11,4 +11,20 @@ public class Zanr implements Serializable {
 
     @Column
     private String naziv;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
 }

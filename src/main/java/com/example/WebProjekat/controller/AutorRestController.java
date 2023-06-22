6d944f2e0c06835bd1d2858b0bc8d743cbbf1e81
Kristina Gudzulic <gudzulic.kristina@gmail.com>
2023-06-22 23:22:55 +0200

    Autor dorada

--- src/main/java/com/example/WebProjekat/controller/AutorRestController.java
+++ src/main/java/com/example/WebProjekat/controller/AutorRestController.java
@@ -1,9 +1,29 @@
 package com.example.WebProjekat.controller;
 
+import com.example.WebProjekat.model.Knjiga;
+import com.example.WebProjekat.service.AutorService;
+import com.example.WebProjekat.service.KnjigaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("api/autori")
+@RequestMapping("autori")
 public class AutorRestController {
+    @Autowired
+    AutorService autorService;
+
+    @Autowired
+    KnjigaService knjigaService;
+
+
+    @GetMapping("{id}/knjige")
+    public ResponseEntity<List<Knjiga>> getKnjige(){
+
+        return null;
+    }
 }
